f9d6b510999ed3eeaa2de82d247f078bc74abf85
Brian Kozeny <bkozen2@uic.edu>
2021-10-03 23:23:14 -0500

    styled item text

--- react-app/src/ItemsList.jsx
+++ react-app/src/ItemsList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -11,12 +10,16 @@ const  ItemList = (props) => {
     return(
         <>
             <Grid container direction="row" alignItems="center" justifyContent="center">
-                <Box display="flex" justifyContent="center" alignItems="center" sx={{width: 500, maxWidth:'100%',}}>
-                    <ul>
+                <Box display="flex" justifyContent="center" alignItems="center" sx={{width: 500, maxWidth:'100%', bgcolor: 'text.secondary'}}>
+                    <List>
                         {props.items.map(item => (
-                            <li key={item.id}>{item.value}</li>
+                            <ListItem key={item.id}>
+                                <Box display="flex" bgcolor="secondary.main" sx={{width: 500, maxWidth:'100%',}}>
+                                    <ListItemText sx={{textAlign: 'center' }}> {item.value} </ListItemText>
+                                </Box>
+                            </ListItem>
                         ))}
-                    </ul>
+                    </List>
                 </Box>
             </Grid>
         </>
